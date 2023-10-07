cbdfbb6107ac70fcc2ddd2e6dc73840b17a330df
Marko Knezevic <markoknez@gmail.com>
2023-10-07 17:51:49 +0200

    new version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='nad_receiver',
-      version='0.3.0',
+setup(name='ha_nad_receiver',
+      version='0.3.1',
       description='Library to interface with NAD receivers through RS232, TCP and telnet',
-      url='https://github.com/joopert/nad_receiver',
-      download_url='https://github.com/joopert/nad_receiver/archive/0.3.0.tar.gz',
-      author='joopert',
+      url='https://github.com/markoknez/ha_nad_receiver',
+      download_url='https://github.com/markoknez/ha_nad_receiver/archive/0.3.1.tar.gz',
+      author='joopert/markoknez',
       license='MIT',
       packages=['nad_receiver'],
       install_requires=['pyserial>=3.2.1'],
